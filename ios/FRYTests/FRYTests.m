56cdd24fca0ff86adf93aeecd37845a98f1232d8
Ahsan Sheikh <mobile.developers02@gmail.com>
2024-08-22 12:11:10 +0500

    Renamed

--- ios/FRYTests/FRYTests.m
+++ ios/FRYTests/FRYTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface YogaCustomerTests : XCTestCase
+@interface FRYTests : XCTestCase
 
 @end
 
-@implementation YogaCustomerTests
+@implementation FRYTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
